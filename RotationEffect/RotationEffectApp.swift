ea2d66e7a9b94b62eb56e4946efa1be93f9ca4bb
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-02 00:50:49 -0700

    Initial Commit

---
+++ RotationEffect/RotationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationEffectApp.swift
+//  RotationEffect
+//
+//  Created by Vadiraj Hippargi on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RotationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
